a1305e3943c85e5e08b2730cc00cebfc10d040e4
gopikrishnareddyR <40296528+gopikrishnareddyR@users.noreply.github.com>
2018-08-01 00:30:09 +0530

    Add files via upload

---
+++ session2_assignment_2_1.R
@@ -0,0 +1,32 @@
+#Session2_assignments_2.1
+
+#1.what are the different meathods to call a function in R?
+#Built in function 
+ source()
+exists()
+mean()
+sapply()
+return()
+and 
+#User defined functions
+function<-function (argument /Logic)
+
+#2. The lazy evaluation of a function means, the argument is evaluated only if it is evaluated only if it is used 
+#inside the body of the function. Say True or False. 
+
+TRUE
+
+
+#3. State True or False: 
+#a. Insights driven from descriptive analytics is not meaningful.
+FALSE
+
+#b. The number of values in each Elements of a list, should be equal. 
+FALSE
+
+#c. The datasets are not stored in memory of the computer using R.
+TRUE
+
+#d. Data frames and matrices are two dimensional however the array is multidimensional. 
+
+TRUE
